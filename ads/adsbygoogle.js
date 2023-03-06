2ec9f9fdc9291de0b8bb868a5014e4e9af20817a
Spider Blogging <88818251+RockBlogger@users.noreply.github.com>
2023-03-06 23:28:28 +0530

    updated minor glitch

--- ads/adsbygoogle.js
+++ ads/adsbygoogle.js
@@ -1,14 +1,6 @@
-/**
-
-    Anti AdBlock - AdBlock Killer
-    Version 3.0
-    Developed by RockBlogger (Telegram: @RockBlogger)
-    For more information visit https://spiderblogging.in
+/*
     
-    
-    This is a Dummy file.
-
-
+   * https://spiderblogging.in
 
 */
   
